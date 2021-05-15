4792b4e64f14a9e2c48086faa62229d852718992
Jochen <jochen-rui@web.de>
2021-05-15 12:24:45 +0200

    added CRUD functionality

--- nestjs-mongodb-backend/src/todo/todo.service.ts
+++ nestjs-mongodb-backend/src/todo/todo.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 import { Todo, TodoDocument } from './schemas/todo.schema';
 
 /**
@@ -19,4 +21,31 @@ export class TodoService {
   constructor(
     @InjectModel(Todo.name) private readonly model: Model<TodoDocument>,
   ) {}
+
+  async findAll(): Promise<Todo[]> {
+    return await this.model.find().exec();
+  }
+
+  async findById(id: string): Promise<Todo> {
+    return await this.model.findById(id).exec();
+  }
+
+  async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    /**
+     * create new Todo object with title, description & createdAt
+     * saves the object in our DB
+     */
+    return await new this.model({
+      ...createTodoDto,
+      createdAt: new Date(),
+    }).save();
+  }
+
+  async update(id: string, updateTodoDto: UpdateTodoDto): Promise<Todo> {
+    return await this.model.findByIdAndUpdate(id, updateTodoDto).exec();
+  }
+
+  async delete(id: string): Promise<Todo> {
+    return await this.model.findByIdAndDelete(id).exec();
+  }
 }
