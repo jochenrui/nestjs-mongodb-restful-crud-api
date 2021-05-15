9b7b0ff7644ad971b03b308206b9d4f6d7fa187e
Jochen <jochen-rui@web.de>
2021-05-15 13:07:02 +0200

    added TodoModule

--- nestjs-mongodb-backend/src/app.module.ts
+++ nestjs-mongodb-backend/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), TodoModule],
 })
 export class AppModule {}
