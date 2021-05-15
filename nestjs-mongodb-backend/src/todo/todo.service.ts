b453650757da98e03427c1fe2615fee8c807e91a
Jochen <jochen-rui@web.de>
2021-05-15 12:19:20 +0200

    inject model into the constructor

--- nestjs-mongodb-backend/src/todo/todo.service.ts
+++ nestjs-mongodb-backend/src/todo/todo.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Todo, TodoDocument } from './schemas/todo.schema';
 
+/**
+ * contains logic for data handling
+ */
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  /**
+   * inject Model as dependency into constructor of class
+   * @InjectModel takes name of our Model as Param
+   * private
+   * readonly
+   * is of type "model" & passed generic type TodoDocument
+   * generic type allows us to access all Mongoose methods for querying, altering & creating data
+   */
+  constructor(
+    @InjectModel(Todo.name) private readonly model: Model<TodoDocument>,
+  ) {}
+}
